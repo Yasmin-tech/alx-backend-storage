c3f9b3bc3188cf015875a99e3f6c268c69964970
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-08-08 13:40:21 +0300

    add the not null constrains to email column

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,6 +3,6 @@
 -- create the schema of the table
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
     );
